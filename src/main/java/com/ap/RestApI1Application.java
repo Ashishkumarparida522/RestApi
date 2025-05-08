5d73a23cc9b3277da7c2fc8bb3475bf9e39b535e
ashish <ashishkumarparida522@gmail.com>
2025-05-08 10:18:20 +0530

    all files are added

---
+++ src/main/java/com/ap/RestApI1Application.java
@@ -0,0 +1,15 @@
+package com.ap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApI1Application {
+
+	public static void main(String[] args) {
+	
+		SpringApplication.run(RestApI1Application.class, args);
+		System.out.println("hi");
+	}
+
+}
